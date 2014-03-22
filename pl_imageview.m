ba93b465e1f1d0c529e4b5543afed5c96a18b93f
Németh Tamás <nemeth@pixlandmedia.com>
2014-03-22 15:47:48 +0100

    First Code Snippet

---
+++ pl_imageview.m
@@ -0,0 +1,11 @@
+// PL ImageView
+// UIImageview
+//
+// IDECodeSnippetCompletionPrefix: __image
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: D745CB69-D378-464D-B6F1-A58208BCA2B7
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+UIImageView* <#variableName#> = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"<#imageName#>"]];
+        
